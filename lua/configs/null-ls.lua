0b0b5b69741a6ec958fefc8a5a807855607fb6f9
asep komarudin <pojokcodeid@gmail.com>
2022-11-29 19:14:07 +0700

    update

--- lua/configs/null-ls.lua
+++ lua/configs/null-ls.lua
@@ -1,1 +1,28 @@
-require("null-ls").setup(astronvim.user_plugin_opts("plugins.null-ls", { on_attach = astronvim.lsp.on_attach }))
+-- require("null-ls").setup(astronvim.user_plugin_opts("plugins.null-ls", { on_attach = astronvim.lsp.on_attach }))
+
+local null_ls_status_ok, null_ls = pcall(require, "null-ls")
+if not null_ls_status_ok then
+  return
+end
+
+-- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
+local formatting = null_ls.builtins.formatting
+-- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
+-- local diagnostics = null_ls.builtins.diagnostics
+
+null_ls.setup({
+  debug = false,
+  sources = {
+    formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
+    formatting.black.with({ extra_args = { "--fast" } }),
+    formatting.stylua,
+    -- diagnostics.flake8
+  },
+
+  on_attach = function(client)
+    vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.format{async=true}")
+    --  if client.resolved_capabilities.document_formatting then
+    -- 	vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting()")
+    -- end
+  end
+})
