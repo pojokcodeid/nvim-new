63303d40da257aa5e102a8c01b40aaa0ebf9ccbd
asep komarudin <pojokcodeid@gmail.com>
2022-11-29 19:39:45 +0700

    update

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -34,7 +34,7 @@ astronvim.vim_opts(astronvim.user_plugin_opts("options", {
     timeoutlen = 300, -- Length of time to wait for a mapped sequence
     undofile = true, -- Enable persistent undo
     updatetime = 300, -- Length of time to wait before triggering the plugin
-    wrap = false, -- Disable wrapping of lines longer than the width of window
+    wrap = true, -- Disable wrapping of lines longer than the width of window
     writebackup = false, -- Disable making a backup before overwriting a file
   },
   g = {
