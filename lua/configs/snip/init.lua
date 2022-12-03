048ad7b5127029c5771d43b88c87ba5be917b579
asep komarudin <pojokcodeid@gmail.com>
2022-12-03 09:24:08 +0700

    update minor

--- lua/configs/snip/init.lua
+++ lua/configs/snip/init.lua
@@ -2,9 +2,16 @@
 -- (probably) in rtp.
 require("luasnip.loaders.from_vscode").lazy_load()
 -- specify the full path...
-require("luasnip.loaders.from_vscode").lazy_load({paths = "C:/Users/Asep/AppData/Local/nvim/my-snippets/B5-Snippets"})
-require("luasnip.loaders.from_vscode").lazy_load({paths = "C:/Users/Asep/AppData/Local/nvim/my-snippets/laravel-blade"})
-require("luasnip.loaders.from_vscode").lazy_load({paths = "C:/Users/Asep/AppData/Local/nvim/my-snippets/laravel-blade2"})
-require("luasnip.loaders.from_vscode").lazy_load({paths = "C:/Users/Asep/AppData/Local/nvim/my-snippets/laravel5"})
+require("luasnip.loaders.from_vscode").lazy_load({ paths = "C:/Users/Asep/AppData/Local/nvim/my-snippets/B5-Snippets" })
+require("luasnip.loaders.from_vscode").lazy_load({
+  paths = "C:/Users/Asep/AppData/Local/nvim/my-snippets/laravel-blade",
+})
+require("luasnip.loaders.from_vscode").lazy_load({
+  paths = "C:/Users/Asep/AppData/Local/nvim/my-snippets/laravel-blade2",
+})
+require("luasnip.loaders.from_vscode").lazy_load({ paths = "C:/Users/Asep/AppData/Local/nvim/my-snippets/laravel5" })
 -- or relative to the directory of $MYVIMRC
-require("luasnip.loaders.from_vscode").load({paths = "./my_snippets/B5-Snippets"})
+require("luasnip.loaders.from_vscode").load({ paths = "C:/Users/Asep/AppData/Local/nvim/my_snippets/B5-Snippets" })
+require("luasnip.loaders.from_vscode").load({ paths = "C:/Users/Asep/AppData/Local/nvim/my_snippets/laravel-blade" })
+require("luasnip.loaders.from_vscode").load({ paths = "C:/Users/Asep/AppData/Local/nvim/my_snippets/laravel-blade2" })
+require("luasnip.loaders.from_vscode").load({ paths = "C:/Users/Asep/AppData/Local/nvim/my_snippets/laravel5" })
