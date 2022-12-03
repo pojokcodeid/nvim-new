89db632df052e40c887b3beca96e8c7834d2983b
asep komarudin <pojokcodeid@gmail.com>
2022-12-03 11:06:06 +0700

    update dynamic path

--- lua/configs/snip/init.lua
+++ lua/configs/snip/init.lua
@@ -1,17 +1,25 @@
--- don't pass any arguments, luasnip will find the collection because it is
--- (probably) in rtp.
+local lpath = vim.fn.stdpath("config") .. "/my-snippets"
+
 require("luasnip.loaders.from_vscode").lazy_load()
 -- specify the full path...
-require("luasnip.loaders.from_vscode").lazy_load({ paths = "C:/Users/Asep/AppData/Local/nvim/my-snippets/B5-Snippets" })
 require("luasnip.loaders.from_vscode").lazy_load({
-  paths = "C:/Users/Asep/AppData/Local/nvim/my-snippets/laravel-blade",
+  paths = lpath .. "/B5-Snippets",
 })
 require("luasnip.loaders.from_vscode").lazy_load({
-  paths = "C:/Users/Asep/AppData/Local/nvim/my-snippets/laravel-blade2",
+  paths = lpath .. "/laravel-blade",
 })
-require("luasnip.loaders.from_vscode").lazy_load({ paths = "C:/Users/Asep/AppData/Local/nvim/my-snippets/laravel5" })
+require("luasnip.loaders.from_vscode").lazy_load({
+  paths = lpath .. "/laravel-blade2",
+})
+require("luasnip.loaders.from_vscode").lazy_load({
+  paths = lpath .. "/laravel5",
+})
+require("luasnip.loaders.from_vscode").lazy_load({
+  paths = lpath .. "/javascript",
+})
 -- or relative to the directory of $MYVIMRC
-require("luasnip.loaders.from_vscode").load({ paths = "C:/Users/Asep/AppData/Local/nvim/my_snippets/B5-Snippets" })
-require("luasnip.loaders.from_vscode").load({ paths = "C:/Users/Asep/AppData/Local/nvim/my_snippets/laravel-blade" })
-require("luasnip.loaders.from_vscode").load({ paths = "C:/Users/Asep/AppData/Local/nvim/my_snippets/laravel-blade2" })
-require("luasnip.loaders.from_vscode").load({ paths = "C:/Users/Asep/AppData/Local/nvim/my_snippets/laravel5" })
+require("luasnip.loaders.from_vscode").load({ paths = lpath .. "/B5-Snippets" })
+require("luasnip.loaders.from_vscode").load({ paths = lpath .. "/laravel-blade" })
+require("luasnip.loaders.from_vscode").load({ paths = lpath .. "/laravel-blade2" })
+require("luasnip.loaders.from_vscode").load({ paths = lpath .. "/laravel5" })
+require("luasnip.loaders.from_vscode").load({ paths = lpath .. "/javascript" })
