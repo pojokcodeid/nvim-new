25b558942e0ecc6ef3043bd3cffe526cad8445c3
asep komarudin <pojokcodeid@gmail.com>
2022-12-11 22:55:52 +0700

    update color scheme

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -417,6 +417,7 @@ local astro_plugins = {
   ["eddyekofo94/gruvbox-flat.nvim"] = {
     config = function()
       vim.o.background = "dark"
+      --vim.g.gruvbox_flat_style = "dark"
       vim.g.gruvbox_flat_style = "hard"
     end,
   },
