4215e04bff393ee1b4dae3135b39bb993e1f34e7
asep komarudin <pojokcodeid@gmail.com>
2022-12-14 08:17:57 +0700

    update

--- lua/user/init.lua
+++ lua/user/init.lua
@@ -45,10 +45,10 @@ local config = {
   options = {
     opt = {
       -- set to true or false etc.
-      relativenumber = true, -- sets vim.opt.relativenumber
-      number = true, -- sets vim.opt.number
-      spell = true, -- sets vim.opt.spell
-      signcolumn = "auto", -- sets vim.opt.signcolumn to auto
+      -- relativenumber = true, -- sets vim.opt.relativenumber
+      -- number = true, -- sets vim.opt.number
+      -- spell = true, -- sets vim.opt.spell
+      -- signcolumn = "auto", -- sets vim.opt.signcolumn to auto
       wrap = true, -- sets vim.opt.wrap
       guifont = "Source Code Pro:h17:w3",
     },
