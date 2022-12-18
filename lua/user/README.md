8066e0dbb3bda71ed83b829847ea1e096fbd161e
asep komarudin <pojokcodeid@gmail.com>
2022-12-18 08:33:41 +0700

    update

---
+++ lua/user/README.md
@@ -0,0 +1,6 @@
+# astro-config
+```
+git clone https://github.com/AstroNvim/AstroNvim ~/.config/nvim
+nvim +PackerSync
+git clone https://github.com/pojokcodeid/astro-config ~/.config/nvim/lua/user
+```
--- lua/user/init.lua
+++ lua/user/init.lua
@@ -15,6 +15,34 @@
 --local status_ok, colorscheme_local = pcall(require, "user.colorscheme.tokyonight-config")
 --if not status_ok then return end
 --require "user.colorscheme.tokyonight-config"
+-- if vim.g.vscode then
+
+--   -- Folding
+--   vim.keymap.set("n", "za", function() vim.fn.VSCodeNotify("editor.toggleFold") end)
+--   vim.keymap.set("n", "zc", function() vim.fn.VSCodeNotify("editor.foldRecursively") end)
+--   vim.keymap.set("n", "zC", function() vim.fn.VSCodeNotify("editor.foldAll") end)
+--   vim.keymap.set("n", "zO", function() vim.fn.VSCodeNotify("editor.unfoldAll") end)
+--   vim.keymap.set("n", "zi", function() vim.fn.VSCodeNotify("yo1dog.cursor-trim.trimCursor") end)
+--   vim.keymap.set("n", "[f", function() vim.fn.VSCodeNotify("workbench.view.search.focus") end)
+--   vim.keymap.set("n", "]f", function() vim.fn.VSCodeNotify("workbench.action.replaceInFiles") end)
+--   vim.keymap.set("n", "gD", function() vim.fn.VSCodeNotify("editor.action.revealDefinitionAside") end)
+--   vim.keymap.set("n", "[p", function() vim.fn.VSCodeNotify("extension.pasteImage") end)
+--   vim.keymap.set("n", "[s", function() vim.fn.VSCodeNotify("editor.action.toggleStickyScroll") end)
+--   vim.keymap.set("n", "=<", function() vim.fn.VSCodeNotify("editor.action.trimTrailingWhitespace") end)
+--   vim.keymap.set("n", "gl", function() vim.fn.VSCodeNotify("editor.action.openLink") end)
+--   vim.keymap.set("n", "<C-k>", function()
+--      vim.fn.VSCodeCall("editor.action.insertLineBefore")
+--      vim.cmd("norm k")
+--   end)
+--   -- vim.keymap.set("n","<leader>", function() vim.fn.VSCodeNotify(""))
+--   -- Visual mode
+--   vim.keymap.set("v", "gs", function() vim.fn.VSCodeNotifyVisual("codesnap.start", true) end)
+--   vim.keymap.set("v", "<", function() vim.fn.VSCodeNotifyVisual("editor.action.outdentLines", false) end)
+--   vim.keymap.set("v", ">", function() vim.fn.VSCodeNotifyVisual("editor.action.indentLines", false) end)
+
+--   -- Insert mode
+--   vim.keymap.set("i", "<C-k>", function() vim.fn.VSCodeNotify("editor.action.insertLineBefore") end)
+-- end
 
 local config = {
 
@@ -281,6 +309,7 @@ local config = {
 
     -- Mapping AKN
     -- Move text up and down
+    
     i = {
       ["<c-c>"] = { '"+y', desc = "" },
       ["<c-v>"] = { "<c-r>+", desc = "" },
